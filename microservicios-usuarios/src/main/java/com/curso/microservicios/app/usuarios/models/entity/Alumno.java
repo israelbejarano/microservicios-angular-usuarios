4f5f3175b5291ba3ac9f71d096874bda8d201547
israel <israel.bejarano.a@gmail.com>
2020-04-23 20:18:07 +0200

    add temporal a createAt

--- microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/models/entity/Alumno.java
+++ microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/models/entity/Alumno.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "alumnos")
@@ -26,6 +28,7 @@ public class Alumno implements Serializable {
 	private String email;
 
 	@Column(name = "create_at")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 
 	@PrePersist
