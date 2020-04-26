ee6c275358c42bb6c9e29d4157c0610a37737dbb
israel <israel.bejarano.a@gmail.com>
2020-04-26 11:51:13 +0200

    Buscar alumnos usando operador like

--- microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/services/AlumnoService.java
+++ microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,10 +1,14 @@
 package com.curso.microservicios.app.usuarios.services;
 
 
+import java.util.List;
+
 import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
 import com.curso.microservicios.commons.services.CommonService;
 
 public interface AlumnoService extends CommonService<Alumno> {
+	
+	public List<Alumno> findByNombreOrApellido(String term);
 	
 
 }
--- microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.curso.microservicios.app.usuarios.services;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.microservicios.app.usuarios.models.repository.AlumnoRepository;
 import com.curso.microservicios.commons.alumnos.models.entity.Alumno;
@@ -9,6 +12,12 @@ import com.curso.microservicios.commons.services.CommonServiceImpl;
 
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Alumno> findByNombreOrApellido(String term) {
+		return repository.findByNombreOrApellido(term);
+	}
 
 
 }
