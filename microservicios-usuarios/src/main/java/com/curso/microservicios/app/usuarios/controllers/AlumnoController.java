fc7654da1a3504a836d2727e8fb4effcf989ada7
israel <israel.bejarano.a@gmail.com>
2020-04-28 12:37:36 +0200

    valid y BindingResult

--- microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/curso/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -2,8 +2,11 @@ package com.curso.microservicios.app.usuarios.controllers;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,7 +21,10 @@ import com.curso.microservicios.commons.controllers.CommonController;
 public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 
 	@PutMapping("/{id}")
-	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id) {
+	public ResponseEntity<?> editar(@Valid @RequestBody Alumno alumno, BindingResult result, @PathVariable Long id) {
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
 		Optional<Alumno> o = service.findById(id);
 		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
